117f3f86c11495fd0694dd78da65b96888224cc4
turbowill4@gmail.com <turbowill4@gmail.com>
2019-03-12 09:08:24 -0400

    ugly v5

--- src/app/services/rss-parser.service.ts
+++ src/app/services/rss-parser.service.ts
@@ -18,77 +18,81 @@ export class RssParserService {
   ) {}
 
   getFeedContent(url: string): Observable<any> {
-    return forkJoin([
-      this.ajaxComm.get({ control: 'XmlParser', params: { url } }).pipe(
-        map(data => {
-          return this.extractFeeds(data, url);
-        })
-      ),
-      this.ajaxComm
-        .get({ control: 'followedPodcasts', params: { rss: url } })
-        .pipe(
-          catchError(err => {
-            return of(null);
+    return this.authService.login('test', 'test').pipe(
+      switchMap(x =>
+        forkJoin([
+          this.ajaxComm.get({ control: 'XmlParser', params: { url } }).pipe(
+            map(data => {
+              return this.extractFeeds(data, url);
+            })
+          ),
+          this.ajaxComm
+            .get({ control: 'followedPodcasts', params: { rss: url } })
+            .pipe(
+              catchError(err => {
+                return of(null);
+              })
+            )
+        ]).pipe(
+          switchMap((result: [Podcast, Podcast]) => {
+            if (result[1]) {
+              return of(result).pipe(
+                map(res => {
+                  const podcast: Podcast = res[0];
+                  podcast.id = res[1].id;
+                  podcast.userId = res[1].userId;
+                  podcast.followed = res[1].followed;
+
+                  podcast.podcastEpisode.forEach((ep: PodcastEpisode) => {
+                    const followedEpisode = res[1].podcastEpisode.find(
+                      x => x.audioUrl === ep.audioUrl
+                    );
+                    ep.id = followedEpisode && followedEpisode.id;
+                    ep.timeStep = followedEpisode && followedEpisode.timeStep;
+                    ep.followedPodcastId = podcast.id;
+                  });
+
+                  return podcast;
+                })
+              );
+            } else {
+              return of(result).pipe(
+                switchMap(res => {
+                  const requestPod: FollowedPodcast = (({
+                    userId,
+                    title,
+                    imageUrl,
+                    rss,
+                    lastListened
+                  }) => ({
+                    userId,
+                    title,
+                    imageUrl,
+                    rss,
+                    followed: false,
+                    lastListened
+                  }))(res[0]);
+
+                  requestPod.userId = this.authService.currentUserValue.id;
+                  console.log(requestPod.userId);
+
+                  return this.ajaxComm
+                    .post({
+                      control: 'FollowedPodcasts',
+                      data: requestPod
+                    })
+                    .pipe(
+                      map(x => {
+                        res[0].id = x.id;
+                        return res[0];
+                      })
+                    );
+                })
+              );
+            }
           })
         )
-    ]).pipe(
-      switchMap((result: [Podcast, Podcast]) => {
-        if (result[1]) {
-          return of(result).pipe(
-            map(res => {
-              const podcast: Podcast = res[0];
-              podcast.id = res[1].id;
-              podcast.userId = res[1].userId;
-              podcast.followed = res[1].followed;
-
-              podcast.podcastEpisode.forEach((ep: PodcastEpisode) => {
-                const followedEpisode = res[1].podcastEpisode.find(
-                  x => x.audioUrl === ep.audioUrl
-                );
-                ep.id = followedEpisode && followedEpisode.id;
-                ep.timeStep = followedEpisode && followedEpisode.timeStep;
-                ep.followedPodcastId = podcast.id;
-              });
-
-              return podcast;
-            })
-          );
-        } else {
-          return of(result).pipe(
-            switchMap(res => {
-              const requestPod: FollowedPodcast = (({
-                userId,
-                title,
-                imageUrl,
-                rss,
-                lastListened
-              }) => ({
-                userId,
-                title,
-                imageUrl,
-                rss,
-                followed: false,
-                lastListened
-              }))(res[0]);
-
-              requestPod.userId = this.authService.currentUserValue.id;
-              console.log(requestPod.userId);
-
-              return this.ajaxComm
-                .post({
-                  control: 'FollowedPodcasts',
-                  data: requestPod
-                })
-                .pipe(
-                  map(x => {
-                    res[0].id = x.id;
-                    return res[0];
-                  })
-                );
-            })
-          );
-        }
-      })
+      )
     );
   }
 
